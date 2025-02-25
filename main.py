bf8c2c0a20f39233742ec98f12dd0802fec89b71
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2025-02-26 02:32:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# - len() function se words ki total count nikal sakte ho.
+
+my_words = ["blue", "red", "purple", "green"]
+len(my_words)
+print(len(my_words))
+
+
+words = ("I Love My Country Pakistan and I am a Pakistani")
+len(words)
+print(len(words))
+
+# ==========================================================
+
+
+# - split() function ka use karke sentence ko words me tod sakte ho.  
+
+sentence = ("I Love Python, Python is my favoirite programming language")
+print(sentence.split())
+
+my_sentence = ("I am  a book lover ")
+print(my_sentence.split())
+# =======================================================
+
+
+  
